4dc415dcacb36d8759f1c1e7ab4e179f4a2704b5
Crispin <crispinkoech.ck@gmail.com>
2019-02-15 16:07:00 +0300

    Interface fields required

--- src/core/poem/interfaces/poem.interface.ts
+++ src/core/poem/interfaces/poem.interface.ts
@@ -1,10 +1,11 @@
 import { Document, Types } from 'mongoose';
 
 interface IPoem {
-    title?: string;
-    verse?: string;
-    poet?: Types.ObjectId;
-    likes?: [Types.ObjectId];
+    title: string;
+    verse: string;
+    poet: Types.ObjectId;
+    likes: [Types.ObjectId];
+    remarks: [Types.ObjectId];
 }
 
 interface MPoem extends IPoem, Document { }
--- src/core/user/interfaces/user.interface.ts
+++ src/core/user/interfaces/user.interface.ts
@@ -1,11 +1,11 @@
 import { Document, Types } from 'mongoose';
 
 interface IUser {
-    email?: string;
-    password?: string;
-    name?: string;
-    verified?: boolean;
-    poems?: [Types.ObjectId];
+    email: string;
+    password: string;
+    name: string;
+    verified: boolean;
+    poems: [Types.ObjectId];
 }
 
 interface MUser extends Document, IUser { }
