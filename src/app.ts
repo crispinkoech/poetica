1add802541066c9bb929d52e58263bc1cdaf39fc
Crispin <crispinkoech.ck@gmail.com>
2019-02-27 16:14:52 +0300

    Separated app file from default entry file

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import feathers from '@feathersjs/feathers';
+import express, { Application } from '@feathersjs/express';
+import socketio from '@feathersjs/socketio';
+import configuration from '@feathersjs/configuration';
+
+import core from './core';
+import database from './utils/database';
+import logger from './utils/logger';
+
+const app: Application<any> = express(feathers());
+
+// Configure application
+app.configure(configuration());
+
+// Set up database, plugins and providers(transports)
+app.configure(express.rest());
+app.configure(socketio);
+app.configure(database);
+
+// Enable body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configure error handler
+app.use(express.errorHandler({ logger }));
+
+// Configure core services
+app.configure(core);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,5 @@
-import feathers from '@feathersjs/feathers';
-import express, { Application } from '@feathersjs/express';
-import socketio from '@feathersjs/socketio';
-import configuration from '@feathersjs/configuration';
-
-import core from './core';
-import database from './utils/database';
+import app from './app';
 import logger from './utils/logger';
-
-const app: Application<any> = express(feathers());
-
-// Configure application
-app.configure(configuration());
-
-// Set up database, plugins and providers(transports)
-app.configure(express.rest());
-app.configure(socketio);
-app.configure(database);
-
-// Enable body parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Configure error handler
-app.use(express.errorHandler({ logger }));
-
-// Configure core services
-app.configure(core);
 
 // Start server
 const host: string = app.get('host');
