d1bfb67800ef02f6e6fc61617082ce6a3319ec5b
Crispin <crispinkoech.ck@gmail.com>
2019-08-02 14:50:45 +0300

    Use production.json

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -6,8 +6,8 @@ import logger from './logger';
 const initialize = (app: Application) => {
     try {
         // Use heroku configuration in production mode
-        const url = app.get('mongodb.url') || process.env.MONGO_URL;
-        const database = app.get('mongodb.database') || process.env.MONGO_DATABASE;
+        const url = app.get('mongodb.url');
+        const database = app.get('mongodb.database');
 
         const options = app.get('mongodb.options');
 
